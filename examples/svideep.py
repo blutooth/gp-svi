1b814c051110da91e3732aa1f35cbf11b50265a5
max <maximilian@live.co.uk>
2016-07-25 16:16:37 +0100

    heyall

--- examples/svideep.py
+++ examples/svideep.py
@@ -1,22 +1,60 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import matplotlib.pyplot as plt
+
+import autograd.numpy as np
+import autograd.numpy.random as npr
+from autograd.numpy.linalg import solve
+import autograd.scipy.stats.multivariate_normal as mvn
+from autograd import value_and_grad
+from scipy.optimize import minimize
+
 
 #2 layer, 1 dimension deep GP
 
-class sparseGP():
-    def __init__(self):
-        self.u1_mean
-        self.u1_cov
-        self.z1
-        self.u2_mean
-        self.u2_cov
-        self.h_mean
-        self.h_cov
-        self.X
-        self.Y
-        self.kernel_noise
-        self.kernel_lenscale
-        self.function_noise
+def sparseGP(x):
+    def unpack(x):
+        u1_mean=
+        u1_cov=
+        z1=
+        u2_mean=
+        u2_cov=
+        h_mean=
+        h_cov=
+        X=
+        Y=
+        kernel_noise=
+        kernel_lenscale=
+        function_noise=
+     return
 
-    def prior(self,x,y):
+
+
+
+    def log_marginal_likelihood(self,params, x, y):
+        mean, cov_params, noise_scale = self.rbf(params)
+        cov_y_y = self.rbf(cov_params, x, x) + noise_scale * np.eye(len(y))
+        prior_mean = mean * np.ones(len(y))
+        return mvn.logpdf(y, prior_mean, cov_y_y, True)
+
+    def predict(self,params, x, y, xstar):
+        """Returns the predictive mean and covariance at locations xstar,
+           of the latent function value f (without observation noise)."""
+        mean, cov_params, noise_scale = unpack_kernel_params(params)
+        cov_f_f = cov_func(cov_params, xstar, xstar)
+        cov_y_f = cov_func(cov_params, x, xstar)
+        cov_y_y = cov_func(cov_params, x, x) + noise_scale * np.eye(len(y))
+        pred_mean = mean +   np.dot(solve(cov_y_y, cov_y_f).T, y - mean)
+        pred_cov = cov_f_f - np.dot(solve(cov_y_y, cov_y_f).T, cov_y_f)
+        return pred_mean, pred_cov
+
+
+    def rbf(kernel_params, x, xp):
+        output_scale = np.exp(kernel_params[0])
+        lengthscales = np.exp(kernel_params[1:])
+        diffs = np.expand_dims(x / lengthscales, 1) \
+                    - np.expand_dims(xp / lengthscales, 0)
+        return output_scale * np.exp(-0.5 * np.sum(diffs ** 2, axis=2))
 
     def sample(self,mu,cov):
 
